44cc9f035fd98a89b3e5cb8051c8b79a02232ece
karl dollmayer <karldollmayer@gmail.com>
2022-03-24 15:50:16 +0100

    update JWT, Validation, Login

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const mongoose = require("mongoose")
+require("dotenv").config()
 const app = express()
 const PORT = 3000
 
 const authRoute =  require('./routes/auth')
+
+
+
+mongoose.connect(process.env.CONNECT_MONGODB, {
+    useNewUrlParser: true
+})
+.then(console.log("Database Up"))
+.catch(err => {
+    console.log(err)
+})
+
+app.use(express.json())
+
 
 app.use('/api/user', authRoute)
 
